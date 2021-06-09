5a8b2485b5ff52e5ee89002bea3920e05783675c
NellaDescodeuses <nellaruscade.pro@gmail.com>
2021-06-09 09:41:06 +0200

    ajout de l'exercice

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+  <title>PHP Exo_1 Part_1</title>
+</head>
+<body>
+  <div class="container">
+    <h1 class="text-danger text-center">## Exercice 1 Partie 1</h1>
+    <h2>Créez une variable et lui affecter une chaîne de caractères. Afficher cette variable.</h2>
+    <p class="text-center mt-4">
+      <?php
+      // je crée ma variable.
+        $hello = "Hello Pépito";
+        echo $hello;
+      ?>
+    </p>
+  </div>
+</html>
+
